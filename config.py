f7611e308063cf77a56088d65ffd0c461f297e75
Christian Sattler <sattler.christian@gmail.com>
2020-11-11 18:42:52 +0100

    Add 'Bearer' to authentication headed. Put authentication token in separate "secret" file.

---
+++ config.py
@@ -0,0 +1,4 @@
+# This is a unique access token for your account.
+# To get one, go to Account -> Settings
+# Scroll down to "Approved integrations" and click "New access token"
+canvas_token = '<-- REPLACE WITH YOUR ACCESS TOKEN -->'
