f2be5983033427940e8e44e425de140bff60f87a
Christian Sattler <sattler.christian@gmail.com>
2020-12-03 02:13:09 +0100

    Finished submission processing tool: process_submission.py

--- canvas_instance.py
+++ canvas_instance.py
@@ -1,7 +1,7 @@
 import logging
 
 from general import OpenWithNoModificationTime, print_json
-from canvas import Canvas, Course, Groups, Assignment
+from canvas import Canvas, Course, GroupSet, Assignment
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
@@ -13,22 +13,22 @@ assignment_id = 'lab 2' #23431
 
 canvas = Canvas('chalmers.instructure.com')
 course = Course(canvas, course_id)
-groups = Groups(canvas, course_id, group_set)
+group_set = GroupSet(canvas, course_id, group_set)
 assignment = Assignment(canvas, course_id, assignment_id)
 
 def user_details(username):
     print('Listing users matching \'{}\':'.format(username))
-    for u in groups.user_name_to_id:
+    for u in group_set.user_name_to_id:
         if username.lower() in u.lower():
-            user = groups.user_name_to_id[u]
-            print('  {} is in {}.'.format(groups.user_str(user), groups.group_str(groups.user_to_group[user]) if user in groups.user_to_group else 'no group.'))
+            user = group_set.user_name_to_id[u]
+            print('  {} is in {}.'.format(group_set.user_str(user), group_set.group_str(group_set.user_to_group[user]) if user in group_set.user_to_group else 'no group.'))
 
 def group_details(groupname):
     groupname = str(groupname)
     print('Listing groups matching \'{}\':'.format(groupname))
-    for g in groups.group_name_to_id:
-        if g.lower() in [x.lower() for x in [groupname, groups.group_prefix + groupname]]:
-            group = groups.group_name_to_id[g]
-            print('  {} has members:'.format(groups.group_str(group)))
-            for user in groups.group_users[group]:
-                print('    {}'.format(groups.user_str(user)))
+    for g in group_set.group_name_to_id:
+        if g.lower() in [x.lower() for x in [groupname, group_set.group_prefix + groupname]]:
+            group = group_set.group_name_to_id[g]
+            print('  {} has members:'.format(group_set.group_str(group)))
+            for user in group_set.group_users[group]:
+                print('    {}'.format(group_set.user_str(user)))
--- process_submissions.py
+++ process_submissions.py
@@ -1,107 +1,133 @@
 import argparse
 import logging
+import os
 from pathlib import Path
 import shlex
+import shutil
 
-from general import print_error, print_json
+from general import print_error, add_to_path
 from canvas import Canvas, GroupSet, Course
 from lab_assignment import LabAssignment
 import submission_fix_lib
 import config
 
-p = argparse.ArgumentParser(
-    description = '\n'.join([
-        'Process student submissions to an assignment on Canvas.',
-        'This involves two stages, \'unpack\' and \'process\', that can be specified as arguments.',
-        'The \'process\' stage caches its results; it can be called iteratively on the same submission working directory.'
-    ])
-)
+dir_script = Path(__file__).parent
+path_extra = dir_script / 'node_modules' / '.bin'
+cache_dir_default = dir_script / 'cache'
 
-p.add_argument('lab', type = int, choices = [1, 2, 3, 4], help = 'The lab to process.')
-p.add_argument('dir', type = Path, help = '\n'.join([
+p = argparse.ArgumentParser(add_help = False, description = '\n'.join([
+    'Process student submissions to an assignment on Canvas.',
+    'This involves two stages, \'unpack\' and \'process\', that can be specified as arguments.',
+    'The \'process\' stage caches its results; it can be called iteratively on the same submission working directory with varying options.'
+]))
+
+g = p.add_argument_group('primary arguments')
+g.add_argument('lab', type = int, choices = [1, 2, 3, 4], help = 'The lab to process.')
+g.add_argument('dir', type = Path, help = '\n'.join([
     'The submission working directory.',
     'This is the base of the hierarchy in which the submissions are collected and processed.',
     'If it does not exist and the submission procession workflow includes unpacking, it will be created.',
     'This directory will be self-contained: it will contain lots of symlinks to reduce file duplication, but they will never point outside the directory.',
     'Thus, you can safely share the directory.'
 ]))
-p.add_argument('--unpack', action = 'store_true', help = '\n'.join([
+g.add_argument('--unpack', action = 'store_true', help = '\n'.join([
     'Collect the submissions and unpack them in the submission working directory.',
-    'This will (re)create the submission working directory.',
-    f'It will create subfolders for each lab group containing \'{LabAssignment.rel_dir_current}\' and \'{LabAssignment.rel_dir_previous}\' subdirectories for the submission of the respective kind.',
+    'This will create the submission working directory if it did not exist.',
+    f'It will (re)create subfolders for each lab group containing \'{LabAssignment.rel_dir_current}\' and \'{LabAssignment.rel_dir_previous}\' subdirectories for the submission of the respective kind.',
     f'It will also as create a \'{LabAssignment.rel_dir_build}\' subdirectory with the current submission overlayed on top of the problem files.',
     'If any submitted files have name not conforming to the expected solution files, an error is raised.',
     f'In that case, the user is provided with information on all such files and a skeleton with which to extend \'name_handlers\' in \'{submission_fix_lib.script_submission_fixes}\' in the lab directory (see documentation in that file).',
 ]))
-p.add_argument('--process', action = 'store_true', help = '\n'.join([
+g.add_argument('--process', action = 'store_true', help = '\n'.join([
     'Assuming the submissions have been unpacked, process them.',
     'This involves the following phases, in order: compilation, testing, pregrading, creating overview index.',
     'Unless changed using --allow-compilation-errors, the workflow will stop after the compilation stage if there where any errors.',
     'This is useful to detect fixable errors such as package declarations and non-existing imports.',
     'You should then extend \'content_handlers\' in \'{submission_fix_lib.script_submission_fixes}\' in the lab directory to persistently fix theses errors (see documentation in that file).',
     'The option --write-ids is useful here.',
-    'Afterwards, you will need to run --unpack again for these fixes to take effect.',
+    'Afterwards, you will need to run --unpack again for these fixes to take effect; to save time, restrict its effect via the --group option to those lab groups impacted by the new content handlers.',
     'In any case, the following phases run only for those lab groups passing compilation.'
     f'Testing uses the tests specified in \'{Path(LabAssignment.rel_dir_test) / LabAssignment.rel_file_tests}\' in the lab directory.',
+    f'It produces output files in the \'{LabAssignment.rel_dir_build_test}\' subdirectory of each lab group folder.',
     f'Pregrading links the java files in \'{Path(LabAssignment.rel_dir_test) / "tests_java"}\' into the build directory and gets its output from the main classes specified in \'{Path(LabAssignment.rel_dir_test) / LabAssignment.rel_file_tests_java}\'.',
-    f'Both phases output files in the \'{LabAssignment.rel_dir_analysis}\' subdirectory of each lab group folder.',
-    'Creation of the overview index outputs further such files and creates a top-level index file that provides an overview over the processed submissions.',
+    f'It produces a text file \'{LabAssignment.rel_file_pregrading}\' in each lab group folder.',
+    f'Creation of the overview index outputs files in the \'{LabAssignment.rel_dir_analysis}\' subdirectory of each lab group folder and references these in a top-level file \'index.html\' that provides an overview over the processed submissions.',
+    f'For this phase, the npm programs \'diff2html-cli\' and \'highlights\' need to be in PATH or {path_extra}.',
+    'If npm is installed, this may be achieved by executing \'npm install diff2html-cli highlights\' in the directory of this script.',
 ]))
 
-p.add_argument('-v', '--verbose', action = 'store_true', help = '\n'.join([
+g = p.add_argument_group('secondary arguments')
+g.add_argument('-h', '--help', action = 'help', help = '\n'.join([
+    'Show this help message and exit.',
+]))
+g.add_argument('-v', '--verbose', action = 'store_true', help = '\n'.join([
     'Print INFO level logging.',
     'This includes accesses to Canvas API endpoints.',
 ]))
-cache_dir_default = Path(__file__).parent / 'cache'
-p.add_argument('--cache-dir', type = str, default = cache_dir_default, help = '\n'.join([
+g.add_argument('--cache-dir', type = str, default = cache_dir_default, help = '\n'.join([
     'The cache directory to use.',
     'If it does not exist, it will be created.',
     f'This defaults to {format(shlex.quote(str(cache_dir_default)))}.',
 ]))
-p.add_argument('--refresh-submissions', action = 'store_true', help = '\n'.join([
+g.add_argument('--refresh-submissions', action = 'store_true', help = '\n'.join([
     'Collect submissions from Canvas instead of the cache.',
     'Use this at the beginning of a submission procession workflow to make sure the cached submissions are up to date.',
     'It is recommended to use this option only then for collecting the submission from Canvas is an expensive operation (on the order of 5 minutes).'
 ]))
-p.add_argument('--refresh-group-set', action = 'store_true', help = '\n'.join([
+g.add_argument('--refresh-group-set', action = 'store_true', help = '\n'.join([
     'Collect group membership information from Canvas instead of the cache.',
     'Use this at the beginning of a submission procession workflow to make sure the cached group memberships are up to date.',
     'Collecting group membership information from Canvas is an expensive operation (on the order of 1 minute).'
 ]))
-p.add_argument('--deadline', type = int, choices = [0, 1, 2], help = '\n'.join([
+g.add_argument('--recreate-swd', action = 'store_true', help = '\n'.join([
+    'Recreate the entire submission wording directory.'
+]))
+g.add_argument('--deadline', type = int, choices = [0, 1, 2], help = '\n'.join([
     'Deadline to use for the overview index file.',
     f'These are specified in \'{LabAssignment.rel_file_deadlines}\' in the lab folder.',
     'If unspecified, information is late submissions will not be recorded in the overview file.'
 ]))
-p.add_argument('--groups', nargs = '+', type = str, help = '\n'.join([
+g.add_argument('--groups', nargs = '+', type = str, help = '\n'.join([
     'Restrict submission processing to these groups.',
     'If omitted, all currently ungraded submissions will be processed.',
 ]))
-p.add_argument('--write-ids', action = 'store_true', help = '\n'.join([
+g.add_argument('--write-ids', action = 'store_true', help = '\n'.join([
     'Together with each submitted file \'<file>\' written in the \'{LabAssignment.rel_dir_current}\' and \'{LabAssignment.rel_dir_previous}\' subdirectories of each lab group, store a file \'.<file>\' containing its Canvas id.',
     'This can be used for easy Canvas id lookup when writing \'content_handlers\' to fix compilation errors.'
 ]))
-p.add_argument('--allow-compilation-errors', action = 'store_true', help = '\n'.join([
+g.add_argument('--allow-compilation-errors', action = 'store_true', help = '\n'.join([
     'Continue the submission procession workflow if there were errors in the compilation stage.',
     'Compilation errors will be listed in the overview document.',
 ]))
-p.add_argument('--no-compilation', action = 'store_true', help = '\n'.join([
+g.add_argument('--no-compilation', action = 'store_true', help = '\n'.join([
     'Skip the compilation phase of the submission procession workflow.',
 ]))
-p.add_argument('--no-tests', action = 'store_true', help = '\n'.join([
+g.add_argument('--no-testing', action = 'store_true', help = '\n'.join([
     'Skip the testing phase of the submission procession workflow.',
 ]))
-p.add_argument('--no-pregrading', action = 'store_true', help = '\n'.join([
+g.add_argument('--no-pregrading', action = 'store_true', help = '\n'.join([
     'Skip the pregrading phase of the submission procession workflow.',
 ]))
-p.add_argument('--no-overview', action = 'store_true', help = '\n'.join([
+g.add_argument('--no-overview', action = 'store_true', help = '\n'.join([
     'Skip creaton of an overview index file at the end of the submission procession workflow.',
 ]))
-p.add_argument('--remove-class-files', action = 'store_true', help = '\n'.join([
+g.add_argument('--remove-class-files', action = 'store_true', help = '\n'.join([
     'After finishing processing, remove all compiled java files from the submission working directory.',
     'Use this option if you plan to share this folder with people who may be running different versions of the Java Development Kit.',
 ]))
+
 args = p.parse_args()
+
+if not (args.unpack or args.dir.exists()):
+    print_error('The submission working directory {} does not exist (and no --unpack option given).'.format(shlex.quote(str(args.dir))))
+    exit(1)
+
+add_to_path(path_extra)
+
+if args.process and not args.no_overview and not (shutil.which('diff2html') and shutil.which('highlights')):
+    print_error('Cannot find the programs \'diff2html\' and \'highlights\'.')
+    print_error('To fix, make sure npm is installed and run \'npm install diff2html-cli highlights\' in the directory of this script.')
+    exit(1)
 
 logging.basicConfig()
 if args.verbose:
@@ -113,6 +139,9 @@ canvas = Canvas(config.canvas_url, cache_dir = Path(args.cache_dir))
 group_set = GroupSet(canvas, config.course_id, config.group_set, use_cache = not args.refresh_group_set)
 lab_assignment = LabAssignment(canvas, config.course_id, args.lab)
 lab_assignment.collect_submissions(use_cache = not args.refresh_submissions)
+
+if args.recreate_swd and args.dir.exists():
+    shutil.rmtree(args.dir)
 
 extra = dict()
 extra['dir'] = args.dir
@@ -126,7 +155,7 @@ if args.unpack:
 if args.process:
     if not args.no_compilation:
         lab_assignment.submissions_compile(strict = not(args.allow_compilation_errors), **extra)
-    if not args.no_tests:
+    if not args.no_testing:
         lab_assignment.submissions_test(**extra)
     if not args.no_pregrading:
         lab_assignment.submissions_pregrade(strict = True, **extra)
