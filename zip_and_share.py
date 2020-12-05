1937e689e0cdd5f7660202496353be00d8d16013
Christian Sattler <sattler.christian@gmail.com>
2020-12-05 22:51:18 +0100

    Added script for zipping and sharing of directories with symlinks (such as submission working directories).

---
+++ zip_and_share.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# PYTHON_ARGCOMPLETE_OK
+
+# We defer expensive initialization to after argument parsing is done.
+# This makes bash completion more responsive.
+import argparse
+from pathlib import Path
+import shlex
+
+import lab_assignment_constants
+
+dir_script = Path(__file__).parent
+cache_dir_default = dir_script / 'cache'
+file_auth_token_default = dir_script / 'auth_token'
+
+p = argparse.ArgumentParser(add_help = False, description = '\n'.join([
+    f'Zip a directory in several ways and upload the result to Canvas.',
+    f'The directory may contain relative symlinks resolving within the directory.',
+]), epilog = '\n'.join([
+    f'This Python script supports bash completion.',
+    f'For this, python-argparse needs to be installed and configured.',
+    f'See https://github.com/kislyuk/argcomplete for more information.',
+]))
+
+g = p.add_argument_group('primary arguments')
+g.add_argument('dir', type = Path, help = '\n'.join([
+    f'The directory to zip and upload to Canvas.',
+]))
+g.add_argument('--zip', action = 'store_true', help = '\n'.join([
+    f'Zip the directory in two different ways.',
+    f'One using zip, preserving the symlinks.',
+    f'One using 7z (with large dictionary size) with symlinks resolved.',
+    f'The zip files have their named derived from the given directory and are stored in its parent directory.'
+    f'Pre-existing zip files of the same name are deleted.'
+]))
+g.add_argument('--share', action = 'store_true', help = '\n'.join([
+    f'Upload the zip files to Canvas.',
+]))
+
+g = p.add_argument_group('secondary arguments')
+g.add_argument('--canvas-folder', type = str, default = 'temp', help = '\n'.join([
+    f'The Canvas folder to store the zip files in.'
+    f'Defaults to \'temp\' in the course root directory.',
+]))
+g.add_argument('--delete-zips', action = 'store_true', help = '\n'.join([
+    f'Delete the produced zip files after they have been uploaded to Canvas.',
+]))
+
+g = p.add_argument_group('general arguments')
+g.add_argument('-h', '--help', action = 'help', help = '\n'.join([
+    f'Show this help message and exit.',
+]))
+g.add_argument('-v', '--verbose', action = 'store_true', help = '\n'.join([
+    f'Print INFO level logging.',
+    f'This includes accesses to Canvas API endpoints.',
+]))
+g.add_argument('--auth-token-file', type = str, default = file_auth_token_default, help = '\n'.join([
+    f'Path to a file storing the Canvas authentication token.',
+    f'Defaults to {shlex.quote(str(file_auth_token_default))}.',
+]))
+g.add_argument('--cache-dir', type = str, default = cache_dir_default, help = '\n'.join([
+    f'The cache directory to use.',
+    f'If it does not exist, it will be created.',
+    f'Defaults to {shlex.quote(str(cache_dir_default))}.',
+]))
+
+#Support for argcomplete.
+try:
+    import argcomplete
+    argcomplete.autocomplete(p)
+except ModuleNotFoundError:
+    pass
+
+args = p.parse_args()
+# Argument parsing is done: expensive initialization can start now.
+
+from collections import namedtuple
+import logging
+import os
+import shutil
+import subprocess
+
+from general import print_error, add_to_path
+from canvas import Canvas, GroupSet, Course
+from lab_assignment import LabAssignment
+import config
+
+# Check directory exists.
+if not args.dir.is_dir():
+    print_error('The given path {} is not a valid directory.'.format(shlex.quote(str(args.dir))))
+    exit(1)
+
+# Check that all the necessary programs are installed.
+if not shutil.which('zip'):
+    print_error('Cannot find \'zip\'.')
+    exit(1)
+if not shutil.which('7z'):
+    print_error('Cannot find \'zip\'.')
+    exit(1)
+
+# Handle verbosity.
+logging.basicConfig()
+if args.verbose:
+    logging.getLogger().setLevel(logging.INFO)
+else:
+    logging.getLogger().setLevel(25)
+
+canvas = Canvas(config.canvas_url, cache_dir = Path(args.cache_dir))
+course = Course(canvas, config.course_id)
+
+# Does not compress well with 'resolve_symlinks = True'.
+def cmd_zip(output, paths, resolve_symlinks = False):
+    return ['zip'] + ([] if resolve_symlinks else ['--symlinks']) + ['-r', str(output), '--'] + list(map(str, paths))
+
+def cmd_7z(output, paths, resolve_symlinks = True):
+    return ['7z', 'a'] + (['-l', '-md=26'] if resolve_symlinks else []) + [str(output), '--'] + list(map(str, paths))
+
+zippers = {
+    'zip': cmd_zip,
+    '7z': cmd_7z,
+}
+
+resolve_symlink_suffix = {
+    True: '',
+    False: '-symlinks',
+}
+
+Config = namedtuple('Config', field_names = ['command', 'resolve_symlinks'])
+
+configs = [
+    Config(command = 'zip', resolve_symlinks = False),
+    Config(command = '7z', resolve_symlinks = True),
+]
+
+def output(c):
+    return args.dir.with_name(args.dir.name + resolve_symlink_suffix[c.resolve_symlinks] + '.' + c.command)
+
+def output_quoted_name(c):
+    return shlex.quote(str(output(c)))
+
+if args.zip:
+    print_error('Zipping...')
+    for c in configs:
+        if output(c).exists():
+            print_error('Deleting pre-existing zip file {}.'.format(output_quoted_name(c)))
+            output(c).unlink()
+        cmd = zippers[c.command](output(c), [args.dir], c.resolve_symlinks)
+        print(cmd)
+        print_error(shlex.join(cmd))
+        subprocess.run(cmd, check = True)
+
+if args.share:
+    # Sanity check.
+    for c in configs:
+        if not output(c).is_file():
+            print_error('The zip file {} is missing.'.format(output_quoted_name(c)))
+            exit(1)
+
+    folder_id = canvas.get_list(course.endpoint + ['folders', 'by_path', args.canvas_folder], use_cache = False)[-1].id
+ 
+    for c in configs:
+        print_error('Uploading {}.'.format(output_quoted_name(c)))
+        file_id = course.post_file(output(c), folder_id, output(c).name, locked = True, use_curl = True)
+        if args.delete_zips:
+            print_error('Deleting zip file {}.'.format(output_quoted_name(c)))
+            output(c).unlink()
