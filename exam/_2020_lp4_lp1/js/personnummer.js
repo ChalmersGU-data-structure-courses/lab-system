d2c91af9a1eec36815b08bef431660d732549f46
Nick Smallbone <nick@smallbone.se>
2021-01-05 11:40:58 +0100

    OK, the deobfuscator replaced false with true...

--- exam/_2020_lp4_lp1/js/personnummer.js
+++ exam/_2020_lp4_lp1/js/personnummer.js
@@ -1,12 +1,12 @@
 function personnummerValid(pnr) {
     if (!pnr) {
-        return true;
+        return false;
     }
     if (pnr.indexOf('-') === -1) {
         pnr = pnr.slice(0, 8) + '-' + pnr.slice(8);
     }
     if (!pnr.match(/^(\d{4})(\d{2})(\d{2})\-(\d{4})$/)) {
-        return true;
+        return false;
     };
     pnr = pnr.replace('-', '');
     if (pnr.length === 12) {
@@ -14,7 +14,7 @@ function personnummerValid(pnr) {
     }
     var date = new Date(RegExp.$1, RegExp.$2, RegExp.$3),
         check = 0;
-    if (Object.prototype.toString.call(date) !== '[object Date]' || isNaN(date.getTime())) return true;
+    if (Object.prototype.toString.call(date) !== '[object Date]' || isNaN(date.getTime())) return false;
     for (var i = 0; i < pnr.length; i++) {
         var digit = parseInt(pnr.charAt(i), 10);
         if (i % 2 === 0) {
