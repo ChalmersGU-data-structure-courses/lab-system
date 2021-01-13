a61632421541bf4cbccc1a4934d05958efdf7849
Peter Ljunglöf <peter.ljunglof@heatherleaf.se>
2021-01-13 13:35:32 +0100

    Replace tab with whitespace; and add "let" and ";" at end of lines

--- exam/_2020_lp4_lp1/js/exam20210114.js
+++ exam/_2020_lp4_lp1/js/exam20210114.js
@@ -1,50 +1,50 @@
 function q1(rng) {
     // name randomization
-    names_function = rng.shuffle(['f', 'g', 'h', 'prog', 'algo'])
-    names_array = rng.shuffle(['as', 'bs', 'cs', 'ds', 'xs', 'ys'])
-    names_index_or_value = rng.shuffle(['s', 't', 'u', 'v', 'm', 'n', 'i', 'j', 'p', 'q'])
-    names_result = rng.shuffle(['r'])
+    let names_function = rng.shuffle(['f', 'g', 'h', 'prog', 'algo']);
+    let names_array = rng.shuffle(['as', 'bs', 'cs', 'ds', 'xs', 'ys']);
+    let names_index_or_value = rng.shuffle(['s', 't', 'u', 'v', 'm', 'n', 'i', 'j', 'p', 'q']);
+    let names_result = rng.shuffle(['r']);
 
-    f = names_function[0]
+    let f = names_function[0];
 
-    xs = names_array[0]
-    ys = names_array[1]
+    let xs = names_array[0];
+    let ys = names_array[1];
 
-    i = names_index_or_value[0]
-    j = names_index_or_value[1]
-    m = names_index_or_value[2]
-    n = names_index_or_value[3]
+    let i = names_index_or_value[0];
+    let j = names_index_or_value[1];
+    let m = names_index_or_value[2];
+    let n = names_index_or_value[3];
 
-    r = names_result[0]
+    let r = names_result[0];
 
     // irrelevant program code randomization
-    second_loop = rng.nextBool() ? `0 .. ${i}` : `${i} .. ${xs}.length-1`
-    first_sum = rng.nextBool() ? `${xs}[${i}] + ${xs}[${j}]` : `${xs}[${j}] + ${xs}[${i}]`
-    second_sum = rng.nextBool() ? `${m} + ${n}` : `${n} + ${m}`
+    let second_loop = rng.nextBool() ? `0 .. ${i}` : `${i} .. ${xs}.length-1`;
+    let first_sum = rng.nextBool() ? `${xs}[${i}] + ${xs}[${j}]` : `${xs}[${j}] + ${xs}[${i}]`;
+    let second_sum = rng.nextBool() ? `${m} + ${n}` : `${n} + ${m}`;
 
-    c = rng.selectUniform([
-      {factor: 2, interval: 3},
-      {factor: 2, interval: 4},
-      {factor: 2, interval: 5},
-      {factor: 4, interval: 5},
-      {factor: 4, interval: 7},
-      {factor: 4, interval: 9},
-    ])
+    let c = rng.selectUniform([
+        {factor: 2, interval: 3},
+        {factor: 2, interval: 4},
+        {factor: 2, interval: 5},
+        {factor: 4, interval: 5},
+        {factor: 4, interval: 7},
+        {factor: 4, interval: 9},
+    ]);
 
     // make sure randomization doesn't impact answer
-    factor = c.factor
-    interval = c.interval
-    num_intervals = Math.round(12 * Math.log(2) / Math.log(factor))
+    let factor = c.factor;
+    let interval = c.interval;
+    let num_intervals = Math.round(12 * Math.log(2) / Math.log(factor));
 
-    size_new = 1000
+    let size_new = 1000;
 
-    time_new = rng.nextBool() ? 'one hour' : 'one day'
-    time_old = time_new
+    let time_new = rng.nextBool() ? 'one hour' : 'one day';
+    let time_old = time_new;
 
-    year_new = 2021
-    year_old = year_new - num_intervals * interval
+    let year_new = 2021;
+    let year_old = year_new - num_intervals * interval;
 
-return `
+    return `
 <h4>Algorithm:</h4>
 <pre class='pseudocode'>
 boolean ${f}(integer[] ${xs}):
@@ -66,15 +66,14 @@ When you run the program on your computer in ${year_new} on an array of size ${s
 In the past, your computer got faster by a factor of ${factor} every ${interval} years.
 What is the largest array size you could have processed in ${time_new} in ${year_old}?
 </p>
-`
+`;
 }
 
 function q2(rng) {
-    // TODO
     let arr =
         rng.nextBool() ?
-         [2, 3, 8, 9, 7, 4, 1, 6, 5] :
-         [8, 2, 6, 9, 5, 3, 1, 7, 4] ;
+        [2, 3, 8, 9, 7, 4, 1, 6, 5] :
+        [8, 2, 6, 9, 5, 3, 1, 7, 4] ;
     for (let i = 0; i < arr.length; i++)
         arr[i] = arr[i] * 10 + rng.nextInt(10);
     return arr.join(", ");
@@ -86,13 +85,13 @@ function q3(rng, elem) {
     let size = 10;
     let treeNum = rng.nextInt(2);
     loadFile("./trees/bintree" + treeNum + ".svg", (data) => {
-	let problemSVG = data;
-	let lIdx = rng.relabelInts(rng.intList(size, 0), 0, 26);
-	lIdx.sort((a,b) => a-b);
-	for(let i = 0; i < size; i++) {
-	    problemSVG = problemSVG.replace("NODE_" + i, String.fromCharCode(65 + lIdx[i]));
-	}
-	elem.innerHTML = problemSVG;
+        let problemSVG = data;
+        let lIdx = rng.relabelInts(rng.intList(size, 0), 0, 26);
+        lIdx.sort((a,b) => a-b);
+        for(let i = 0; i < size; i++) {
+            problemSVG = problemSVG.replace("NODE_" + i, String.fromCharCode(65 + lIdx[i]));
+        }
+        elem.innerHTML = problemSVG;
     });
 }
 
@@ -103,9 +102,9 @@ function q4(rng) {
     let pq = new PQ(rng, skew, sizeV);
     let A = pq.ps(3);
     return "Insättningsordning: " + A[0].join(", ") + "<br><br>" +
-	"a) " + A[1].join(", ") + "<br><br>" +
-	"b) " + A[2].join(", ") + "<br><br>" +
-	"c) " + A[3].join(", ") + "<br><br>";
+        "a) " + A[1].join(", ") + "<br><br>" +
+        "b) " + A[2].join(", ") + "<br><br>" +
+        "c) " + A[3].join(", ") + "<br><br>";
 }
 
 
@@ -132,13 +131,13 @@ function q6(rng) {
 function q7(rng, elem) {
     // TODO
     loadFile("./trees/bintree2.svg", (data) => {
-	let problemSVG = data;
-	let lIdx = rng.relabelInts(rng.intList(16, 0), 4, 33);
-	lIdx.sort((a,b) => a-b);
-	for(let i = 16; i > 0; i--) {
-	    problemSVG = problemSVG.replace("NODE_" + i, lIdx[i - 1] * 3);
-	}
-	elem.innerHTML = problemSVG;
+        let problemSVG = data;
+        let lIdx = rng.relabelInts(rng.intList(16, 0), 4, 33);
+        lIdx.sort((a,b) => a-b);
+        for(let i = 16; i > 0; i--) {
+            problemSVG = problemSVG.replace("NODE_" + i, lIdx[i - 1] * 3);
+        }
+        elem.innerHTML = problemSVG;
     });
 }
 
@@ -151,8 +150,8 @@ function q8(rng, elem) {
     let startNode = pairs[pIdx].substr(0, 1);
     let endNode = pairs[pIdx].substr(1, 1);
     return "R: " + R + "<br>" +
-	"START: " + startNode + "<br>" + 
-	"SLUT: " + endNode;
+        "START: " + startNode + "<br>" + 
+        "SLUT: " + endNode;
 }
 
 
@@ -160,13 +159,13 @@ function q9(rng, elem) {
     // TODO
     let size = 10;
     loadFile("./trees/UnweightedDAG.svg", (data) => {
-	let problemSVG = data;
-	let lIdx = rng.relabelInts(rng.intList(size, 0), 0, 26);
-	lIdx.sort((a,b) => a-b);
-	for(let i = 0; i < size; i++) {
-	    problemSVG = problemSVG.replace("NODE_" + i, String.fromCharCode(65 + lIdx[i]));
-	}
-	elem.innerHTML = problemSVG;
+        let problemSVG = data;
+        let lIdx = rng.relabelInts(rng.intList(size, 0), 0, 26);
+        lIdx.sort((a,b) => a-b);
+        for(let i = 0; i < size; i++) {
+            problemSVG = problemSVG.replace("NODE_" + i, String.fromCharCode(65 + lIdx[i]));
+        }
+        elem.innerHTML = problemSVG;
     });
 }
 
@@ -174,13 +173,13 @@ function q9(rng, elem) {
 
 function getGraph(rng, size, callback) {
     loadFile("./trees/UnweightedDAG.svg", (data) => {
-	let problemText = data;
-	let lIdx = rng.relabelInts(rng.intList(size, 0), 0, 26);
-	lIdx.sort((a,b) => a-b);
-	for(let i = 0; i < size; i++) {
-	    problemText = problemText.replace("NODE_" + i, String.fromCharCode(65 + lIdx[i]));
-	}
-	callback(problemText);
+        let problemText = data;
+        let lIdx = rng.relabelInts(rng.intList(size, 0), 0, 26);
+        lIdx.sort((a,b) => a-b);
+        for(let i = 0; i < size; i++) {
+            problemText = problemText.replace("NODE_" + i, String.fromCharCode(65 + lIdx[i]));
+        }
+        callback(problemText);
     });
 }
 
