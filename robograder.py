7530a1221eb63a0bc9e593a5f8aa4aac08117420
Christian Sattler <sattler.christian@gmail.com>
2021-02-13 21:35:46 +0100

    Fix bugs.

--- robograder.py
+++ robograder.py
@@ -9,15 +9,20 @@ from lab import *
 logger = logging.getLogger('robograder')
 
 logging.basicConfig()
-logger.setLevel(logging.DEBUG)
+logging.getLogger().setLevel(logging.INFO)
 
 import gitlab_config
 course = Course(gitlab_config)
-lab = Lab(course, 2, bare = True)
 
-lab.print_unhandled_tests()
+lab1 = Lab(course, 1, bare = True)
+lab2 = Lab(course, 2, bare = True)
+lab3 = Lab(course, 3, bare = True)
 
-lab.update_grading_repo()
-lab.robograde_tests()
+lab2.print_unhandled_tests()
+lab2.update_grading_repo()
+lab2.robograde_tests()
+lab2.print_handled_tests()
 
-lab.print_handled_tests()
+lab1.update_submissions_and_gradings()
+lab2.update_submissions_and_gradings()
+lab3.update_submissions_and_gradings()
