952ae5b3b767d38f52b17c254803c833b799c977
Peter Ljunglöf <peter.ljunglof@heatherleaf.se>
2021-05-25 23:43:11 +0200

    Individualisation of question 6, exam 2021-06-03

---
+++ exam/_2021_06_03_exam/question6.py
@@ -0,0 +1,49 @@
+
+import itertools
+import random
+
+
+NEDGES = 9
+NCHEAP = 5  # setting to 6 makes the problem harder for the students
+
+def test_unique_path(weights, path):
+    for n in range(1, len(weights) + 1):
+        for comb in itertools.combinations(weights, n):
+            if sum(comb) == sum(path):
+                if set(path) != set(comb):
+                    # print(f"Retrying: {' + '.join(map(str,path))} == {' + '.join(map(str,comb))} == {sum(path)}")
+                    return False
+    return True
+
+
+def question6(seed=None):
+    rnd = random.Random(seed)
+    weights = None
+    while not (weights and test_unique_path(weights, path)):
+        # numbers 1 2 ... 9
+        weights = list(range(1, NCHEAP + NEDGES))
+        # remove a random number until the length == NCHEAP
+        while len(weights) > NCHEAP:
+            del weights[rnd.randrange(0, len(weights))]
+        # the path will be the even-numbered elements 
+        path = weights[0::2]
+        # the cheap distractors are the odd-numbered elements
+        cheap = weights[1::2]
+        path_cost = sum(path)
+
+    weights.append(path_cost)
+    while len(weights) < NEDGES:
+        weights.append(weights[-1] + rnd.randrange(1, 4))
+
+    mst_cost = sum(weights[:5])
+
+    return {
+        'weights': ', '.join(map(str, weights)),
+        'path_cost': path_cost,
+        'mst_cost': mst_cost,
+    }
+
+
+if __name__=='__main__':
+    print(question6())
+
