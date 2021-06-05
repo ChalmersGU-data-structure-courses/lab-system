6651a0822eaf65ed58e54c74bdf27086d7367022
Christian Sattler <sattler.christian@gmail.com>
2021-06-05 11:12:14 +0200

    Integrated Pelle's solution generator for Q5.

--- exam/_2021_06_03_exam/question5.py
+++ exam/_2021_06_03_exam/question5.py
@@ -5,17 +5,29 @@ import subprocess
 
 this_dir = Path(__file__).parent
 
+name = 'HashTables20210604'
+
+def parse_output(s):
+    for line in s.splitlines():
+        a = line.split('=')
+        if len(a) == 2:
+            yield(a[0], a[1].strip('"'))
+
+def compile():
+    java.compile_java([this_dir / (name + '.java')])
+
+def run_and_parse(args):
+    cmd = list(java.java_cmd(name, args))
+    output = subprocess.run(cmd, check = True, capture_output = True, encoding = 'utf-8', cwd = this_dir).stdout
+    yield from parse_output(output)
+
 class Generator:
     def __init__(self, seed):
         r = random.Random(seed)
-        java_seed = r.randrange(1000000)
-        name = 'HashTables20210603'
-        java.compile_java([this_dir / (name + '.java')])
-        cmd = list(java.java_cmd(name, [str(java_seed)]))
-        self.output = subprocess.run(cmd, check = True, capture_output = True, encoding = 'utf-8', cwd = this_dir).stdout
+        self.java_seed = r.randrange(1000000)
+        compile()
 
     def replacements(self, solution = False):
-        for line in self.output.splitlines():
-            a = line.split('=')
-            if len(a) == 2:
-                yield(a[0], a[1].strip('"'))
+        yield from run_and_parse([str(self.java_seed)])
+        if solution:
+            yield from run_and_parse([str(self.java_seed), 'solution'])
