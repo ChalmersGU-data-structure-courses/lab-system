2cc314dbf7b53279dd1ef452ac0d5464f3837077
Christian Sattler <sattler.christian@gmail.com>
2021-08-18 14:33:47 +0200

    Clarification.

--- exam/_2021_08_27/question_template.py
+++ exam/_2021_08_27/question_template.py
@@ -1,12 +1,11 @@
-# To write a generator for question N, copy this file to question_N.py and fill in the below class.
-
+# To write a generator for question N, copy this file to question_N.py in the same directory and fill in the below class.
 import random
 
 class Generator:
     def __init__(self, seed):
         self.r = random.Random(seed)
 
-    # A generator function returning key-value replacement pairs.
+    # A generator function returning key-value string replacement pairs.
     # The boolean parameter 'solution' indicates if this is for the exam or solution document.
     # This function is called only once per class instance, so it is safe to mutate self.r.
     def replacements(self, solution = False):
