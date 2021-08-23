9c641fe046d250e67f2a94d656d7a2a76e83bad3
Christian Sattler <sattler.christian@gmail.com>
2021-08-23 15:38:56 +0200

    Added documentation for replacements_img in generator template.

--- exam/_2021_08_27/question_template.py
+++ exam/_2021_08_27/question_template.py
@@ -1,11 +1,14 @@
 # To write a generator for question N, copy this file to questionN.py in the same directory and fill out the below class.
+# import pathlib
 import random
+# import tempfile
 
 class Generator:
     # Use 'seed' to initialize random number generation.
     # For pregenerated problems, use the version number (ranges from 0 to number of versions) to select the problem.
     def __init__(self, seed, version = None):
         self.r = random.Random(seed)
+        self.version = version
 
     # A generator function returning key-value string replacement pairs.
     # The boolean parameter 'solution' indicates if this is for the exam or solution document.
@@ -15,3 +18,16 @@ class Generator:
         yield ('key_1', 'B')
         if solution:
             yield ('key_solution_0', 'B')
+
+    # Implement this generator function to replace placeholder images in the exam or solution document by generated images.
+    # Parameters are as for the method 'replacements'.
+    # Yield key-value pairs of a string id of the placeholder image and the local path to an image to replace it.
+    # The caller takes ownership of the image and will delete it.
+    # def replacements_img(self, solution = False):
+    #     (fd, name) = tempfile.mkstemp(suffix = '.png')
+    #     os.close(fd)
+    #     path = pathlib.Path(name)
+    #
+    #     # generate custom image and store it under 'path'
+    #
+    #     yield ('kix.gq7jtslsbc2f', path) # replace id by actual id
