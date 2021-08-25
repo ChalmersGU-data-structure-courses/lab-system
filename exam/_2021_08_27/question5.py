8326b703e67076626ce59eeec5d6807330812bca
Nick Smallbone <nick@smallbone.se>
2021-08-25 12:02:20 +0200

    Add question 5

---
+++ exam/_2021_08_27/question5.py
@@ -0,0 +1,14 @@
+import random
+
+class Generator:
+    def __init__(self, seed, version = None):
+        self.r = random.Random(seed)
+        self.version = version
+
+    def replacements(self, solution = False):
+        keys = ["a", "b", "c", "d", "e"]
+        values = ["", "1", "2", "3", "4"]
+        assert len(keys) == len(values)
+
+        permuted = self.r.sample(["", "1", "2", "3", "4"], 5)
+        return dict(zip(keys, permuted))
