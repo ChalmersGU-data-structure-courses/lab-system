9b0f3bb3906bba9ff3d236efea233f8d71887078
Christian Sattler <sattler.christian@gmail.com>
2021-08-26 12:32:06 +0200

    Shuffle versions in Q3 in case max_versions for that question is less than the maximum.

--- exam/_2021_08_27/question3.py
+++ exam/_2021_08_27/question3.py
@@ -1,7 +1,13 @@
 import os
 import pathlib
 import random
+import re
 import tempfile
+
+def shuffled(r, xs):
+    ys = list(xs)
+    r.shuffle(ys)
+    return ys
 
 def gen_tmp_file(suffix = None):
     (fd, name) = tempfile.mkstemp(suffix = suffix)
@@ -10,13 +16,28 @@ def gen_tmp_file(suffix = None):
 
 class Generator:
     def __init__(self, seed, version):
-        self.r = random.Random(seed)
         self.version = version
+
+        self.image_dir = pathlib.Path(__file__).parent / 'question3'
+        self.image_pattern = 'BST-(\\d+).png'
+        
+        def f():
+            for path in self.image_dir.iterdir():
+                m = re.fullmatch(self.image_pattern, path.name)
+                if m:
+                    yield (int(m.group(1)), path)
+        self.versions = dict(f())
+        assert sorted(self.versions.keys()) == list(range(len(self.versions)))
+
+        self.version_order = shuffled(random.Random('half-empty bottle'), self.versions.keys())
+        self.version_shuffled = self.version_order[self.version]
 
     def replacements_img(self, solution = False):
         path = gen_tmp_file(suffix = '.png')
         with tempfile.TemporaryDirectory() as dir:
             link = pathlib.Path(dir) / 'link'
-            link.symlink_to(pathlib.Path(__file__).parent.resolve() / 'question3' / f'BST-{self.version:0>2d}.png')
+            link.symlink_to(self.versions[self.version_shuffled].resolve())
             link.rename(path)
         yield ('kix.c5e9tjg3u8t7', path)
+
+g = Generator(0, 3)
