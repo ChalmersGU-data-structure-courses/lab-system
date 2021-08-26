9fcc67da8db79f80610248a8eaa59a493e9ccb77
Christian Sattler <sattler.christian@gmail.com>
2021-08-26 23:35:41 +0200

    Create separate versions of exam for DIT181 and LET375 with link to course API modified. Add data file for DIT961 from Alex.

--- exam/_2021_08_27/dit181/data.py
+++ exam/_2021_08_27/dit181/data.py
@@ -7,7 +7,7 @@ import general
 
 this_dir = Path(__file__).parent
 
-exam_id = '10-CDGIaOvx-pjQH5kSyf6U337Mf39EQpa2376HlyEjI'
+exam_id = '1rxsF75uardtvgERWVEjBCckFw0Qh7GAm3mBnaegOodc'
 solution_id = None #'1hJbk5HOzRp8Q76MIEh5VTvObZH4wAxA4hVXAOg6iBTs'
 secret_salt = 'Rainy night'
 
--- exam/_2021_08_27/let375/data.py
+++ exam/_2021_08_27/let375/data.py
@@ -9,7 +9,7 @@ import general
 
 this_dir = Path(__file__).parent
 
-exam_id = '10-CDGIaOvx-pjQH5kSyf6U337Mf39EQpa2376HlyEjI'
+exam_id = '1cl4Vr4dFEivyMh2t7tvxMb2IcZ5yZsP5ObraVQwDtQk'
 solution_id = None #'1hJbk5HOzRp8Q76MIEh5VTvObZH4wAxA4hVXAOg6iBTs'
 secret_salt = 'Rainy night'
 
