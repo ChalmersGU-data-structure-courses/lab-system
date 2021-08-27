a6a8ce8bc02d512eb41397697d2eda30a51cc865
Christian Sattler <sattler.christian@gmail.com>
2021-08-27 19:47:15 +0200

    Solution generator for Question 3.

--- exam/_2021_08_27/question3.py
+++ exam/_2021_08_27/question3.py
@@ -13,11 +13,27 @@ def gen_tmp_file(suffix = None):
     os.close(fd)
     return pathlib.Path(name)    
 
-def format_direction(is_left):
+def format_left(is_left):
     return 'left' if is_left else 'right'
 
+def format_L(is_left):
+    return 'L' if is_left else 'R'
+
+def format_smaller(is_left):
+    return 'smaller' if is_left else 'larger'
+
+def format_smallest(is_left):
+    return 'smallest' if is_left else 'largest'
+
 def svg_template_initial_tree(is_left):
-    return image_dir / f'BST-{format_direction(is_left)}.svg'
+    return image_dir / f'BST-{format_left(is_left)}.svg'
+
+def svg_template_inserted_tree(is_left):
+    return image_dir / f'BST-{format_left(is_left)}-inserted.svg'
+
+def svg_template_removed_tree(is_left, intended):
+    suffix_intended = 'a' if intended else 'b'
+    return image_dir / f'BST-{format_left(is_left)}-removed-{suffix_intended}.svg'
 
 # svg_template is a function taking a boolean 'is_left' as argument and returning a path to the SVG file.
 # The result is a string.
@@ -33,9 +49,10 @@ def png_from_svg(png_path, svg):
             check = True,
         )
 
-def png_from_svg_template(png_path, *args):
-    x = instantiate_svg_template(*args)
-    png_from_svg(png_path, x)
+def gen_png_from_svg_template(*args):
+    png_path = gen_tmp_file(suffix = '.png')
+    png_from_svg(png_path, instantiate_svg_template(*args))
+    return png_path
 
 class _Generator:
     def __init__(self, placeholder_id, seed, version):
@@ -44,7 +61,6 @@ class _Generator:
         self.r = random.Random(seed)
         self.is_left = self.r.choice([True, False])
         self.nodes = sorted(self.r.sample(string.ascii_uppercase, 9), reverse = not self.is_left)
-        self.params = [self.is_left, self.nodes]
 
     def replacements(self, solution = False):
         for (i, node) in enumerate(self.nodes):
@@ -52,12 +68,18 @@ class _Generator:
 
         if solution:
             for v in [True, False]:
-                yield (format_direction(v), format_direction(v == self.is_left))
+                yield (format_left(v), format_left(v == self.is_left))
+                yield (format_L(v), format_L(v == self.is_left))
+                yield (format_smaller(v), format_smaller(v == self.is_left))
+                yield (format_smallest(v), format_smallest(v == self.is_left))
 
     def replacements_img(self, solution = False):
-        path = gen_tmp_file(suffix = '.png')
-        png_from_svg_template(path, svg_template_initial_tree, *self.params)
-        yield (self.placeholder_id, path)
+        yield (self.placeholder_id, gen_png_from_svg_template(svg_template_initial_tree, self.is_left, self.nodes))
+
+        if solution:
+            yield ('kix.fb9j64evdz0y', gen_png_from_svg_template(svg_template_inserted_tree, self.is_left, self.nodes))
+            for v in [True, False]:
+                yield ('kix.r7dc08ncuqle' if v else 'kix.7d5i67igq28c', gen_png_from_svg_template(svg_template_inserted_tree, self.is_left, self.nodes))
 
 def Generator(placeholder_id):
     def f(seed, version = None):
