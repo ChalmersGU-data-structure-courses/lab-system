b69ba580a3e842849c0ecf80272f3385d5a74fbe
Christian Sattler <sattler.christian@gmail.com>
2021-10-28 13:01:14 +0200

    Adjust log level in grader invitation executable.

--- invite_graders.py
+++ invite_graders.py
@@ -11,5 +11,7 @@ logging.getLogger().setLevel(logging.INFO)
 # The Java and Python courses share a single graders group.
 # This is managed by the Java course.
 course = Course(config)
+course.logger.setLevel(logging.DEBUG)
+
 course.canvas_course_refresh()
 course.invite_teachers_to_gitlab(this_dir / 'teacher_invitations')
