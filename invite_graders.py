56c914d310b7c890e4d2d3c0c8eca7533b577ceb
Christian Sattler <sattler.christian@gmail.com>
2021-10-28 12:54:17 +0200

    Add executable script for inviting graders to GitLab.

---
+++ invite_graders.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import logging
+
+from course import Course
+import java.gitlab_config as config
+from this_dir import this_dir
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+
+# The Java and Python courses share a single graders group.
+# This is managed by the Java course.
+course = Course(config)
+course.canvas_course_refresh()
+course.invite_teachers_to_gitlab(this_dir / 'teacher_invitations')
