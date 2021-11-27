f74f710d68581556d64e7928b2bc82bf6b529ce8
Christian Sattler <sattler.christian@gmail.com>
2021-11-27 12:44:13 +0100

    Refactor chardet functions from general into chardet_tools to minimize dependencies in general

---
+++ chardet_tools.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+import chardet
+
+# Detection seems not to be so good.
+# A Unicode file with 'Markus Järveläinen' is detected as EUC-KR.
+def detect_encoding(files):
+    detector = chardet.universaldetector.UniversalDetector()
+    for file in files:
+        if isinstance(file, str):
+            file = Path(file)
+        detector.feed(file.read_bytes())
+    detector.close()
+    return detector.result['encoding']
+
+def read_text_detect_encoding(path):
+    try:
+        return path.read_text()
+    except UnicodeDecodeError:
+        return path.read_text(encoding = detect_encoding([path]))
--- general.py
+++ general.py
@@ -1,4 +1,3 @@
-import chardet
 from collections import defaultdict, namedtuple
 import contextlib
 from datetime import datetime, timedelta, timezone
@@ -518,23 +517,6 @@ def wait_and_check(process, cmd):
     r = process.wait()
     if r != 0:
         raise subprocess.CalledProcessError(r, cmd)
-
-# Detection seems not to be so good.
-# A Unicode file with 'Markus Järveläinen' is detected as EUC-KR.
-def detect_encoding(files):
-    detector = chardet.universaldetector.UniversalDetector()
-    for file in files:
-        if isinstance(file, str):
-            file = Path(file)
-        detector.feed(file.read_bytes())
-    detector.close()
-    return detector.result['encoding']
-
-def read_text_detect_encoding(path):
-    try:
-        return path.read_text()
-    except UnicodeDecodeError:
-        return path.read_text(encoding = detect_encoding([path]))
 
 def read_without_comments(path):
     return list(filter(lambda s: s and not s.startswith('#'), path.read_text().splitlines()))
