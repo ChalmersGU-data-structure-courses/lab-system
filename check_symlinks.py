ffd45a1197f943d07a0419d64f80bd12f22a9a67
Christian Sattler <sattler.christian@gmail.com>
2021-11-30 23:08:18 +0100

    Improvements to check_symlinks: * Add strict symlink checking (forbidding all symlinks). Make exceptions instances of markdown.Markdown.

--- check_symlinks.py
+++ check_symlinks.py
@@ -5,47 +5,51 @@ import general
 import markdown
 
 
-# Lesson learned:
-# The resolution of a path is not transparent in the following sense.
-# Earlier symlinks in the path are resolved before later symlinks are interpreted.
-# So the following statement is false:
-# a/link resolves to a/target whenever link points to target.
-
-class SymlinkException(Exception):
+class SymlinkException(Exception, markdown.Markdown):
     def __init__(self, path):
         self.path = path
+        self.path_formatted = general.format_path(path)
 
-class AbsoluteSymlinkException(SymlinkException):
-    def __init__(self, path):
-        super().__init__(path)
-
+class HasSymlinkException(SymlinkException):
     def __str__(self):
-        return 'The symlink {} refers to an absolute path.'.format(general.format_path(self.path))
+        return f'There is a symlink {self.path_formatted}.'
 
     def markdown(self):
         return general.join_lines([
-            'The symlink'
-        ]) + markdown.escape_code_block(str(self.path)) + general.join_lines([
-            'refers to an absolute path.'
+            'There is a symlink',
+            *markdown.escape_code_block(str(self.path)).splitlines(),
+            'and symlinks are forbidden.'
+        ])
+
+class AbsoluteSymlinkException(SymlinkException):
+    def __str__(self):
+        return f'The symlink {self.path_formatted} refers to an absolute path.'
+
+    def markdown(self):
+        return general.join_lines([
+            'The symlink',
+            *markdown.escape_code_block(str(self.path)).splitlines(),
+            'refers to an absolute path.',
         ])
 
 class EscapingSymlinkException(SymlinkException):
-    def __init__(self, path):
-        super().__init__(path)
-
     def __str__(self):
-        return 'The symlink {} refers to a path outside the top-level directory.'.format(
-            general.format_path(self.path),
+        return (
+            f'The symlink {self.path_formatted} refers to '
+            'a path outside the top-level directory.'
         )
 
     def markdown(self):
         return general.join_lines([
-            'The symlink'
-        ]) + markdown.escape_code_block(str(self.path)) + general.join_lines([
-            'refers to a path outside the top-level directory.'
+            'The symlink',
+            *markdown.escape_code_block(str(self.path)).splitlines(),
+            'refers to a path outside the top-level directory.',
         ])
 
-def check_link(link):
+def check_link(link, strict):
+    if strict:
+        raise HasSymlinkException(link)
+
     target = link.readlink()
     if target.is_absolute():
         raise AbsoluteSymlinkException(link)
@@ -54,13 +58,21 @@ def check_link(link):
     if len(xs) >= 1 and xs[0] == '..':
         raise EscapingSymlinkException(link)
 
-def check_self_contained_in_current_dir(dir):
+def check_in_current_dir(dir, strict):
     for path in dir.iterdir():
         if path.is_dir():
-            check_self_contained_in_current_dir(path)
+            check_in_current_dir(path)
         elif path.is_symlink():
-            check_link(path)
+            check_link(path, strict)
 
-def check_self_contained(dir):
+def check(dir, strict = False):
+    '''
+    Check that all symlinks in the directory 'dir' are non-problematic.
+    Raise an instance of SymlinkException if a problem is found.
+
+    The meaning of "non-problematic" depends on 'strict'.
+    If True, no symlinks are allowed.
+    If False, only symlinks that do not escape from the specified directory are allowed.
+    '''
     with general.working_dir(dir):
-        check_self_contained_in_current_dir(Path())
+        check_in_current_dir(Path(), strict)
