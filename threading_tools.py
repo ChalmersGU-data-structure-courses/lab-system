946969c19dfe5b36e4d7160be12f1b50ecb0eab4
Christian Sattler <sattler.christian@gmail.com>
2021-12-08 10:19:49 +0100

    Move and add general threading tools to new module threading_tools.

---
+++ threading_tools.py
@@ -0,0 +1,32 @@
+import contextlib
+import logging
+import threading
+
+import general
+
+
+logger = logging.getLogger(__name__)
+
+# From https://stackoverflow.com/a/48741004.
+class RepeatTimer(threading.Timer):
+    def run(self):
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
+
+@contextlib.contextmanager
+def thread_manager(thread):
+    thread.start()
+    try:
+        yield
+    finally:
+        logger.debug(f'waiting for thread {thread.name} to join...')
+        thread.join()
+        logger.debug('thread {thread.name} has joined')
+
+@contextlib.contextmanager
+def timer_manager(timer):
+    def cleanup():
+        logger.debug(f'cancelling timer {timer.name}')
+        timer.cancel()
+
+    return general.add_cleanup(thread_manager(timer), cleanup)
