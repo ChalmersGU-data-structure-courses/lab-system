178349ef2263fb0e43f2f8dc4bbc458e80062057
Christian Sattler <sattler.christian@gmail.com>
2022-01-29 13:14:38 +0000

    Enable Lab 2.

--- dit182/config.py
+++ dit182/config.py
@@ -288,7 +288,7 @@ def _lab_item(k, *args):
 # Dictionary sending lab identifiers to lab configurations.
 labs = dict([
     _lab_item(1, 'sorting-complexity'  , datetime.timedelta(minutes = 15)),  # noqa: E203
-#    _lab_item(2, 'autocomplete'        , datetime.timedelta(minutes = 15)),  # noqa: E203
+    _lab_item(2, 'autocomplete'        , datetime.timedelta(minutes = 15)),  # noqa: E203
 #    _lab_item(3, 'plagiarism-detection', datetime.timedelta(minutes = 15)),  # noqa: E203
 #    _lab_item(4, 'path-finder'         , datetime.timedelta(minutes = 15)),  # noqa: E203
 ])
