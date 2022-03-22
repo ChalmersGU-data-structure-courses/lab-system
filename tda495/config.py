3bc284dd5b7046c1cdc475a0509b48f8d6098b72
Christian Sattler <sattler.christian@gmail.com>
2022-03-22 16:23:36 +0100

    Only enable the submission system for Lab 1 for now.

--- tda495/config.py
+++ tda495/config.py
@@ -348,9 +348,9 @@ def _lab_item(k, *args):
 # Dictionary sending lab identifiers to lab configurations.
 labs = dict([
     _lab_item(1, 'sorting-complexity'  , datetime.timedelta(minutes = 15)),
-    _lab_item(2, 'autocomplete'        , datetime.timedelta(minutes = 15)),
-    _lab_item(3, 'plagiarism-detection', datetime.timedelta(minutes = 15)),
-    _lab_item(4, 'path-finder'         , datetime.timedelta(minutes = 15)),
+#    _lab_item(2, 'autocomplete'        , datetime.timedelta(minutes = 15)),
+#    _lab_item(3, 'plagiarism-detection', datetime.timedelta(minutes = 15)),
+#    _lab_item(4, 'path-finder'         , datetime.timedelta(minutes = 15)),
 ])
 
 # Students taking part in labs who are not registered on Canvas.
