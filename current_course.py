234c52b6df1aa2ae3b1c879d51f67f3bb1c2ff91
Christian Sattler <sattler.christian@gmail.com>
2022-03-22 16:29:06 +0100

    Fix wrong course code.

--- current_course.py
+++ current_course.py
@@ -3,10 +3,10 @@ import logging
 import course
 
 from this_dir import this_dir
-import tda495.config as config
+import dat495.config as config
 
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
 
-c = course.Course(config, dir = this_dir / 'tda495')
+c = course.Course(config, dir = this_dir / 'dat495')
