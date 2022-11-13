f85897b4f0d79a69ee5cb80a1613b85741e753d7
Christian Sattler <sattler.christian@gmail.com>
2022-11-13 02:17:45 +0100

    Use straight diffs on Chalmers GitLab.

--- lab_handlers_python.py
+++ lab_handlers_python.py
@@ -19,20 +19,15 @@ class TestingColumn(live_submissions_table.Column):
     def get_value(self, group):
         submission_current = group.submission_current(deadline = self.config.deadline)
         solution_test_tag = self.lab.submission_solution.repo_tag(segments_test_tag)
-        tag_after = submission_current.repo_tag_after_create(
-            'solution',
-            solution_test_tag,
-            segments_test,
-        )
-        url = gitlab_tools.url_compare(
-            self.lab.grading_project.get,
-            solution_test_tag,
-            tag_after,
-        )
+        current_test_tag = submission_current.repo_tag(segments_test_tag)
 
         def format_cell(cell):
             with cell:
-                live_submissions_table.format_url('solution..', url)
+                live_submissions_table.format_url('solution..', gitlab_tools.url_compare(
+                    self.lab.grading_project.get,
+                    solution_test_tag,
+                    current_test_tag,
+                ))
         return live_submissions_table.CallbackColumnValue(callback = format_cell)
 
 class SubmissionHandler(lab_handlers.SubmissionHandler):
