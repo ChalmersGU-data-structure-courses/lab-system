c9a412d83d5c3a3e93880019d7e504a8debd160f
Christian Sattler <sattler.christian@gmail.com>
2023-01-10 14:02:57 +0100

    Add script to watch for course name changes.

---
+++ one_off_scripts/observe_course_name.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import logging
+from pathlib import Path
+import sys
+import time
+
+sys.path.append(str(Path('__file__').parent / '..'))
+
+import canvas  # noqa: E402
+import general  # noqa: E402
+
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(
+    format = '%(asctime)s %(levelname)s %(module)s: %(message)s',
+    datefmt = '%Y-%m-%d %H:%M:%S',
+    level = logging.WARN,
+)
+
+from gitlab_config_personal import canvas_auth_token  # noqa: E402
+c = canvas.Canvas('chalmers.instructure.com', auth_token = canvas_auth_token)
+course_id = 23356
+desired_name = 'LP3 Data structures and algorithms'
+desired_course_code = 'DIT182 and DAT495'
+
+while True:
+    details = c.get(['courses', course_id])
+    if not (details.name == desired_name and details.course_code == desired_course_code):
+        logger.warn(general.join_lines([
+            'Course details changed:',
+            f'* name: {details.name}',
+            f'* course code: {details.course_code}',
+        ]))
+        c.put(['courses', course_id], {
+            'course[name]': desired_name,
+            'course[course_code]': desired_course_code,
+        })
+    time.sleep(10)
