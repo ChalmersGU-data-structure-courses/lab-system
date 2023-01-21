595fb635967dfbef0adc9ef0b997fb1cc42b1421
Christian Sattler <sattler.christian@gmail.com>
2023-01-21 18:03:33 +0100

    Add script.

---
+++ one_off_scripts/fix_usernames_with_extra_1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import logging
+from pathlib import Path
+import sys
+
+import gitlab
+
+sys.path.append(str(Path('__file__').parent / '..'))
+
+logging.basicConfig(
+    format = '%(asctime)s %(levelname)s %(module)s: %(message)s',
+    datefmt = '%Y-%m-%d %H:%M:%S',
+    level = logging.WARNING,
+)
+logger = logging.getLogger(__name__)
+
+import course
+import gitlab_tools  # noqa: E402
+import lp3.config as config
+
+c = course.Course(config, 'lp3')
+
+for user in c._gitlab_users.values():
+    username = user.username
+    if username.endswith('1') and not username.startswith('project'):
+        cid = username.strip('1')
+        print(f"    '({cid}', '{username}'),")
