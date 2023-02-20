46d8fa4d63d6e1f90b06ce15d01492fa3b9d3d41
Lab system <>
2023-02-20 23:52:25 +0100

    Enable Lab 4.

--- lp3/config.py
+++ lp3/config.py
@@ -388,8 +388,8 @@ def _lab_item(k, *args, **kwargs):
 labs = dict([
     _lab_item(1, 'binary-search'       , datetime.timedelta(minutes = 60), has_robograder = True),  # noqa: E203
     _lab_item(2, 'indexing'            , datetime.timedelta(minutes = 15), has_robograder = True),  # noqa: E203
-    _lab_item(3, 'plagiarism-detection', datetime.timedelta(minutes = 30), has_robograder = True),  # noqa: E203
-    #_lab_item(4, 'path-finder'         , datetime.timedelta(minutes = 15)),  # noqa: E203
+    _lab_item(3, 'plagiarism-detection', datetime.timedelta(minutes = 15), has_robograder = True),  # noqa: E203
+    _lab_item(4, 'path-finder'         , datetime.timedelta(minutes = 30), has_robograder = True),  # noqa: E203
 ])
 
 # Students taking part in labs who are not registered on Canvas.
