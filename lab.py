23419d1b18a9a6a4a2b927a63589567e056b670b
Christian Sattler <sattler.christian@gmail.com>
2023-12-05 08:44:41 +0100

    Create grading projects in Lab.deploy_via_lab_sources_and_canvas.
    This catches more errors than waiting for them to be created lazily when running event loop.

--- lab.py
+++ lab.py
@@ -1626,6 +1626,11 @@ class Lab:
         self.logger.info('=== Forking student projects from official project ===')
         self.create_group_projects()
 
+        if self.config.grading_via_merge_request:
+            self.logger.info('=== Creating grading projects ===')
+            for g in l.student_groups.values():
+                g.grading_via_merge_request.project.create()
+
         self.logger.info(
             '''
             Note: students are not yet added/invited to lab projects.
