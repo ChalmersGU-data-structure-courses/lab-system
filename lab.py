36f62d466624c2be2bb21fa058cc758c61a63646
Christian Sattler <sattler.christian@gmail.com>
2024-02-23 16:47:49 +0100

    Add function for unprotecting spuriously protected main branches.

--- lab.py
+++ lab.py
@@ -681,6 +681,12 @@ class Lab:
         for problem in self.heads_problem:
             gitlab_.tools.protect_branch(self.gl, project, problem)
 
+    def unprotect_main_branches(self):
+        for g in self.groups.values():
+            with gitlab_.tools.exist_ok():
+                g.project.lazy.protectedbranches.delete('main')
+                self.logger.info(f'WARNING: unprotected branch main for group {g.name}')
+
     def create_group_projects(self, exist_ok = False):
         for group in self.groups_known():
             x = group.project
