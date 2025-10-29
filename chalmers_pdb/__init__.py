514e5775ee4db07efd2ef98ff717633a378b7f87
Christian Sattler <sattler.christian@gmail.com>
2025-10-29 05:55:21 +0100

    Turn PDB client into subpackage.
    TODO: fix usage

---
+++ chalmers_pdb/__init__.py
@@ -0,0 +1,2 @@
+# pylint: disable=unused-import
+from .tools import Auth, Client, PDBException
--- chalmers_pdb/tools.py
+++ chalmers_pdb/tools.py
@@ -1,21 +1,36 @@
+import dataclasses
+
+import util.print_parse
 from chalmers_pdb.new_rpcc_client import RPCC
-from gitlab_config_personal import pdb_login
 
 
-pdb = RPCC("https://pdb.chalmers.se:4434")
-pdb.login(*pdb_login)
+@util.print_parse.dataclass_json
+@dataclasses.dataclass(frozen=True, kw_only=True)
+class Auth:
+    username: str
+    password: str
 
 
 class PDBException(LookupError):
     pass
 
 
-def personnummer_to_cid(personnummer):
-    try:
-        rs = pdb.account_dig({"cid_of": {"all_pnrs": personnummer}}, {"name": True})
-        (r,) = rs
-        return r["name"]
-    except (KeyError, ValueError) as e:
-        raise PDBException(
-            f"Could not obtain CID for personnummer {personnummer}: got {rs}"
-        ) from e
+class Client:
+    pdb: RPCC
+
+    def __init__(self, *, url: str = "https://pdb.chalmers.se:4434", auth: Auth):
+        self.pdb = RPCC(url)
+        self.pdb.login(user=auth.username, password=auth.password)
+
+    def personnummer_to_cid(self, personnummer):
+        try:
+            rs = self.pdb.account_dig(
+                {"cid_of": {"all_pnrs": personnummer}},
+                {"name": True},
+            )
+            (r,) = rs
+            return r["name"]
+        except (KeyError, ValueError) as e:
+            raise PDBException(
+                f"Could not obtain CID for personnummer {personnummer}: got {rs}"
+            ) from e
