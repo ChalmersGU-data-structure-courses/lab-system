4be873daf2ba8e6858aafabd8699bb8bee429cca
Christian Sattler <sattler.christian@gmail.com>
2025-12-16 13:35:32 +0100

    DAT076: disable unused systems, streamline config.

--- dat076/config.py
+++ dat076/config.py
@@ -9,25 +9,16 @@ Search for ACTION to find locations where you need to take action.
 The default lab configuration is as needed for the data structures course cluster.
 """
 
-import datetime
 import re
 from pathlib import PurePosixPath
 
-import grading_sheet.config
-# import handlers.java
-# import handlers.python
-# import handlers.variants
-# import util.enum
 import util.print_parse
-# import util.this_dir
+
 from lab_interfaces import (
     CourseConfig,
     DefaultLabId,
-    DefaultOutcome,
     GroupSetConfig,
     LabConfig,
-    LabIdConfig,
-    OutcomesConfig,
 )
 
 
@@ -42,21 +33,10 @@ group_set: GroupSetConfig[GroupId] = GroupSetConfig[GroupId](
 )
 
 
-# Outcomes
-# --------
-
-Outcome = DefaultOutcome
-
-outcomes: OutcomesConfig[Outcome] = OutcomesConfig.from_enum_spec(Outcome)
-
-
 # Lab ids
 # -------
 
 LabId = DefaultLabId
-
-lab_id = LabIdConfig()
-
 
 # Labs
 # ----
@@ -65,22 +45,18 @@ lab_id = LabIdConfig()
 def lab_item(
     id: LabId,
     name: str,
-    refresh_minutes: int,
 ) -> tuple[LabId, LabConfig]:
     lab_config = LabConfig(
         name_semantic=name,
         group_set=group_set,
-        outcomes=outcomes,
-        request_handlers={"submission": handlers.general.SubmissionHandlerStub()},
-        refresh_period=datetime.timedelta(minutes=refresh_minutes),
-        canvas_assignment_name=f"{lab_id.name.print(id)}: {name}",
+        request_handlers={},
     )
     return (id, lab_config)
 
 
 labs: list[tuple[LabId, LabConfig]] = [
-    # id, name, refresh_minutes
-    lab_item(1, "Project", 15),
+    # id, name
+    lab_item(1, "Project"),
 ]
 
 
@@ -93,13 +69,8 @@ course: CourseConfig
 course = CourseConfig(
     canvas_domain="chalmers.instructure.com",
     canvas_course_id=38042,
-    canvas_grading_path=PurePosixPath() / "lab-system",
     gitlab_path=gitlab_path,
     gitlab_path_graders=gitlab_path / "graders",
-    grading_spreadsheet=grading_sheet.config.ConfigExternal(
-        spreadsheet="1XwNA7SiQoT9ZGxTMW11j7OWF1QBl6r-Tdov6wFq1KxM"
-    ),
-    lab_id=lab_id,
     labs=dict(labs),
     chalmers_id_to_gitlab_username_override={
         "aarne": "Aarne.Ranta",
