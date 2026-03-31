b3d9a679d09ca8ac864cb7a6ea9f53da078abb6b
Christian Sattler <sattler.christian@gmail.com>
2026-03-31 11:52:10 +0200

    Fix #92.

--- chalmers_pdb/tools.py
+++ chalmers_pdb/tools.py
@@ -1,7 +1,10 @@
 import dataclasses
+from pathlib import Path
 
 import util.print_parse
-from chalmers_pdb.new_rpcc_client import RPCC
+
+from .new_rpcc_client import RPCC
+from .cached_client import CachedClient
 
 
 @util.print_parse.dataclass_json
@@ -16,42 +19,54 @@ class PDBException(LookupError):
 
 
 class Client:
-    pdb: RPCC
+    client: CachedClient
 
-    def __init__(self, *, url: str = "https://pdb.chalmers.se:4434", auth: Auth):
-        self.pdb = RPCC(url)
-        self.pdb.login(user=auth.username, password=auth.password)
+    def __init__(
+        self,
+        *,
+        url: str = "https://pdb.chalmers.se:4434",
+        auth: Auth,
+        cache_dir: Path = Path("cache_pdb"),
+    ):
+        def client_constructor():
+            client = RPCC(url)
+            client.login(user=auth.username, password=auth.password)
+            return client
+
+        self.client = CachedClient(client_constructor, cache_dir=cache_dir)
 
     def personnummer_to_cid(self, personnummer):
-        try:
-            rs = self.pdb.account_dig(
+        def get(update: bool = False):
+            return self.client.account_dig(
                 {"cid_of": {"all_pnrs": personnummer}},
                 {"name": True},
+                update=update,
             )
-            (r,) = rs
-            return r["name"]
-        except (KeyError, ValueError) as e:
-            raise PDBException(
-                f"Could not obtain CID for personnummer {personnummer}: got {rs}"
-            ) from e
+
+        for update in [False, True]:
+            rs = get(update)
+            try:
+                (r,) = rs
+                return r["name"]
+            except (KeyError, ValueError) as e:
+                if update is False:
+                    continue
+
+                raise PDBException(
+                    f"Could not obtain CID for personnummer {personnummer}: got {rs}"
+                ) from e
 
     def details(self, cid):
-        try:
-            rs = self.pdb.person_dig(
-                {"cid": {"name": cid}},
-                {
-                    "name": True,
-                    "nid": True,
-                    "gu_email": True,
-                    "gu_ids": True,
-                    "gu_pnr": True,
-                    "l3_email": True,
-                    "official_emails": True,
-                    "official_phones": True,
-                },
-            )
-            return rs
-        except (KeyError, ValueError) as e:
-            raise PDBException(
-                f"Could not obtain details for CID {cid}: got {rs}"
-            ) from e
+        return self.client.person_dig(
+            {"cid": {"name": cid}},
+            {
+                "name": True,
+                "nid": True,
+                "gu_email": True,
+                "gu_ids": True,
+                "gu_pnr": True,
+                "l3_email": True,
+                "official_emails": True,
+                "official_phones": True,
+            },
+        )
